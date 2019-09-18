190582abccb97a4126070ba8dfc4af9749fd4072
adam0va <whereisthefire@yandex.ru>
2019-09-18 21:32:12 +0300

    dubug false

--- task1/settings.py
+++ task1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c9at!chswo-j$0117estrv46c)kqytwa=y8yrgx(dt%7sut=l+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'rsoi1-2019.herokuapp.com']
 
@@ -110,7 +110,7 @@ renderer_classes = ['rest_framework.renderers.JSONRenderer']
 
 if DEBUG:
     renderer_classes.append('rest_framework.renderers.BrowsableAPIRenderer')
-    
+
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': renderer_classes,
 }
