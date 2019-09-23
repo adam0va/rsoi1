ce129b514942ea74b4b43e57af6390c911ab8bf2
adam0va <whereisthefire@yandex.ru>
2019-09-23 18:51:55 +0300

    add tests

--- people/tests.py
+++ people/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from people.models import Person
+from rest_framework.test import APIClient
+from django.test.runner import DiscoverRunner
+
+class NoDbTestRunner(DiscoverRunner):
+
+    def setup_databases(self, **kwargs):
+        """ Override the database creation defined in parent class """
+        pass
+
+    def teardown_databases(self, old_config, **kwargs):
+        """ Override the database teardown defined in parent class """
+        pass
+
+class PeopleTestCase(TestCase):
+    def setUp(self):
+         self.client = APIClient()
+    def test_get(self):
+        result = self.client.get('/people/')
+        self.assertNotEqual(len(result.json()), 0)
 
 # Create your tests here.
--- task1/settings.py
+++ task1/settings.py
@@ -130,6 +130,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+TEST_RUNNER = 'people.tests.NoDbTestRunner'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
